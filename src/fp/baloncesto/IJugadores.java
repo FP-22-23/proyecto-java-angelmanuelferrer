4a38e4fbe2cca22b04e3e6ef8f017b6274c5f589
Angel Manuel Ferrer Alvarez <angelfaa19@gmail.com>
2023-05-19 16:16:56 +0200

    Entrega 3

--- src/fp/baloncesto/IJugadores.java
+++ src/fp/baloncesto/IJugadores.java
@@ -2,6 +2,8 @@ package fp.baloncesto;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
 
 public interface IJugadores {
 	
@@ -15,4 +17,24 @@ public interface IJugadores {
 	
 	public Map<Posiciones, Integer> puntosPorPosicion();
 	
+	public Boolean existenAllStarStream();
+	
+	public Integer contarJugadoresDeEquipoStream(String equipo);
+	
+	public Set<Jugador> jugadoresDePosicionStream(String posicion);
+	
+	public Integer maximosPuntosEquipo(String equipo);
+	
+	public SortedSet<Jugador> jugadoresPosicionOrdenadosPuntos(String posicion);
+	
+	public Map<String, Set<Jugador>> jugadoresPorUniversidadStream();
+	
+	public Map<Posiciones, Double> mediaPuntosPorPosiciones();
+	
+	public Map<Posiciones,Integer> maximaEdadPorPosicion();
+	
+	public SortedMap<String, Set<Jugador>> nMejoresJugadoresPorEquipo(Long n);
+	
+	public Integer edadMaxima();
+
 }
